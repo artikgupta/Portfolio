9890d14aa4244a384e42d2fd034582c6d44c8319
artikgupta <artig9262@gmail.com>
2020-08-14 18:08:10 +0530

    clean up- add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import Home from "./components/home/index";
+import Header from "./components/header/index";
+import About from "./components/about";
+import Resume from "./components/resume/index";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Portfolio</h1>
-      </header>
-      <Home />
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/project" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
